6e88fa0d69cd0ae70c225978f82913792527d311
useEffects <joel.sr1024@gmail.com>
2023-12-09 19:51:26 +0530

    use <img>

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import { DarkModeContext } from "@/context/darkmode"
-import Image from "next/image"
 import { useContext } from "react"
 
 export function Logo() {
     const { darkMode } = useContext(DarkModeContext)
-    return <Image src={darkMode ? "/hyperd_dark.png" : "/hyperd_light.png"} className="w-24 h-24 object-contain" alt="logo" />
+    return <img src={darkMode ? "/hyperd_dark.png" : "/hyperd_light.png"} className="w-24 h-24 object-contain" alt="logo" />
 }
