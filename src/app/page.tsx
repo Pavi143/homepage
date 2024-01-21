85703952439e897043b1c831d0c92750ef93e040
pavi143 <vspavipavi@gmail.com>
2024-01-21 19:47:25 +0530

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,34 +22,32 @@ export default function Page() {
   const isMobile = useMediaQuery("(max-width:640px)")
   const [repoCount, setRepoCount] = useState<number | null>(null);
   const [membersCount, setMembersCount] = useState<number | null>(null);
+
   useEffect(() => {
     const fetchRepoCount = async () => {
       try {
-        const response = await axios.get(`https://api.github.com/orgs/coding-club-gct`);
-        setRepoCount(response.data.public_repos);
-        console.log(`response is ${response}`)
+        const response = await fetch(`https://api.github.com/orgs/coding-club-gct`, {
+          headers: {
+            "Authorization": `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
+          }
+        }).then(res => res.json());
+        setRepoCount(response.public_repos);
       } catch (error) {
         console.error('Error fetching organization repository count:', error);
       }
     };
+    async function fetchMemberCount() {
+      await fetch("/api").then(res => res.json()).then(data => setMembersCount(data.approximate_member_count));
+    }
 
     fetchRepoCount();
+    fetchMemberCount()
   }, []);
 
-  useEffect( () => {
-    async function fetchMemberCount() {
-     await fetch("/api").then(res => res.json()).then(data => setMembersCount(data.approximate_member_count));
-    }
-    fetchMemberCount()
-  }, [])
-
-
-
-
-  return <div className="flex">
-    <div >
-      <div >
-        <Container className="flex flex-col md:flex-row justify-around md:mt-28  md:ml-10">
+  return <div>
+    <div>
+      <div className="flex justify-center pt-12" >
+        <Container className="flex flex-col md:flex-row justify-around ">
           <div className="w-full mt-12 md:mt-0 md:w-1/3 md:h-1/3">
             <img src={logo.src} className="object-contain w-full">
             </img>
@@ -88,31 +86,34 @@ export default function Page() {
 
           </div>
         </Container>
+
       </div>
-      <div className="flex flex-col md:flex-row  gap-8 m-4 pt-8 w-full bg-mantle p-4">
-        <div className="flex flex-col gap-4 md:w-1/2 ml-4">
-          <p className="text-3xl text-subtext0 font-medium text-center  mt-4">VISION</p>
-          <p className="mt-6">To build a community that serves as a hub for young programmers to learn, develop and enrich their coding skills and also acts as a platform through which they can exhibit their skills to the technical world as talented programmers..</p>
-        </div>
-        <div className="flex flex-col gap-4 md:w-1/2 ml-4 mb-4 ">
-          <p className="text-3xl font-medium text-center mt-4 text-subtext0">MISSION</p>
-          <div className="flex">
-            <div className="flex flex-col gap-16 md:gap-8 mt-8 ml-2">
-              <div className="flex ">
-                <FontAwesomeIcon icon={faCode} className="m-1"></FontAwesomeIcon>
-                <p className="pl-4">To elevate the programming skills of future engineers which will help them learn new tactics to deal with dynamic technologies.</p>
-              </div>
-              <div className="flex">
-                <FontAwesomeIcon icon={faCode} className="m-1"></FontAwesomeIcon>
-                <p className="pl-4">To enrich the competitive programming skills of students by participating in renowned competitions.</p>
-              </div>
-              <div className="flex ">
-                <FontAwesomeIcon icon={faCode} className="m-1"></FontAwesomeIcon>
-                <p className="pl-4">To enrich the competitive programming skills of students by participating in renowned competitions.</p>
+      <div className="flex justify-center w-full bg-mantle p-4" >
+        <Container className="flex flex-col md:flex-row  gap-8  ">
+          <div className="flex flex-col gap-4 md:w-1/2 ml-4">
+            <p className="text-3xl text-subtext0 font-medium text-center  mt-4">VISION</p>
+            <p className="mt-6">To build a community that serves as a hub for young programmers to learn, develop and enrich their coding skills and also acts as a platform through which they can exhibit their skills to the technical world as talented programmers..</p>
+          </div>
+          <div className="flex flex-col gap-4 md:w-1/2 ml-4 mb-4 ">
+            <p className="text-3xl font-medium text-center mt-4 text-subtext0">MISSION</p>
+            <div className="flex">
+              <div className="flex flex-col gap-16 md:gap-8 mt-8 ml-2">
+                <div className="flex ">
+                  <FontAwesomeIcon icon={faCode} className="m-1"></FontAwesomeIcon>
+                  <p className="pl-4">To elevate the programming skills of future engineers which will help them learn new tactics to deal with dynamic technologies.</p>
+                </div>
+                <div className="flex">
+                  <FontAwesomeIcon icon={faCode} className="m-1"></FontAwesomeIcon>
+                  <p className="pl-4">To enrich the competitive programming skills of students by participating in renowned competitions.</p>
+                </div>
+                <div className="flex ">
+                  <FontAwesomeIcon icon={faCode} className="m-1"></FontAwesomeIcon>
+                  <p className="pl-4">To enrich the competitive programming skills of students by participating in renowned competitions.</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
 
       <p className="text-4xl md:my-8 mx-4  my-12 md:pt-8 text-subtext0">Lorem ipsum dolor sit.</p>
