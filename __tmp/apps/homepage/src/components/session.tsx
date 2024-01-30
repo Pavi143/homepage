1fe48207956a22319197a6fdcec9e08a627bd768
aprilia <joel.sr1024@gmail.com>
2024-01-31 01:37:34 +0530

    folder reorder

--- __tmp/apps/homepage/src/components/session.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import { SessionProvider as SP } from "next-auth/react";
-import { ReactNode } from "react";
-
-export default function SessionProvider({ children }: { children: ReactNode }) {
-    return <SP> {children} </SP>
-}
