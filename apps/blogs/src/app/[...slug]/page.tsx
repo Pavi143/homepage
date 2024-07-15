d4f9bb85ddbeacd88716cde0b231cdbc50172adc
useEffects <joel.sr1024@gmail.com>
2024-07-16 03:39:32 +0530

    folder typo

--- apps/blogs/src/app/[...slug]/page.tsx
+++ apps/blogs/src/app/[...slug]/page.tsx
@@ -60,6 +60,7 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
     if (!repo) return new NextResponse("Internal Server Error", { status: 500 })
 
     const allFiles = await fetchAllFiles(repo, blog.basePath)
+    console.log(allFiles)
     const markdown = await fetchMarkdown(params, repo, blog.basePath)
 
     if (!markdown) return notFound()
@@ -164,7 +165,7 @@ const BlogHeader = async ({ frontmatter, repo }: { frontmatter: FrontMatterData,
     return <div className="w-full py-4 flex flex-col gap-8">
         <div className="flex flex-col gap-8 [&>*]:px-4 max-w-6xl mx-auto">
             <p className="text-3xl md:text-5xl font-bold">{title}</p>
-            <img src={finalCoverImage} className="w-full object-contain !px-0"/>
+            <img src={finalCoverImage} className="w-full object-contain !px-0" />
             <div className="flex flex-wrap gap-4 items-center">
                 {finalTags.map((tag, i) => <span key={i} className="bg-surface0 px-2 py-1 rounded-xl">{tag}</span>)}
             </div>
